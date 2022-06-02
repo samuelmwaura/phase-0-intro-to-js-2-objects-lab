3103926a5b32645eed9a4bb28cb88df0d53c3283
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-06-03 01:31:15 +0300

    Completed the assignment.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name:'Kimotho',
+    streetAdrress:'102 Kubernetes'
+}
+
+function updateEmployeeWithKeyAndValue(employeeObj,key,value){
+   const newObj={...employeeObj}
+
+   newObj[key]=value;
+
+   return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObj,key,value){
+    employeeObj[key]=value;
+    return employeeObj;
+}
+
+function deleteFromEmployeeByKey(employeeObj,key){
+    const newObj= {...employeeObj};
+    delete newObj[key];
+
+    return newObj;
+}
+
+ function destructivelyDeleteFromEmployeeByKey(employeeObj,key){
+    delete employeeObj[key];
+
+    return employeeObj;
+}
